08e9f90fa774c476c2ed12b4f7a0e8ffc1ad859c
Luiz Fernando <luizfsilva1705@gmail.com>
2025-03-26 17:39:05 -0300

    Alteração do limie

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -21,7 +21,7 @@ if ('speechSynthesis' in window) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1','Jogo do Número Secreto');
-    exibirTextoNaTela('p','Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p','Escolha um número entre 1 e 50');
     
 }
 
